4b09f499ee771f8e1004c42ceab4757045976deb
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:06:18 -0500

    build (Users.Api): created value object Password

---
+++ Users.Api/Domain/Common/Password.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Text.RegularExpressions;
+using ValueOf;
+
+namespace Users.Api.Domain.Common
+{
+    public class Password : ValueOf<string, Password>
+    {
+        private static readonly Regex PasswordRegex =
+            new("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        protected override void Validate()
+        {
+            if (!PasswordRegex.IsMatch(Value))
+            {
+                var message = $"{Value} is not a valid password";
+                throw new ValidationException(message, new[]
+                {
+                    new ValidationFailure(nameof(Password), message)
+                });
+            }
+        }
+    }
+}
