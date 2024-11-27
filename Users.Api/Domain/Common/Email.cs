4a9ae11599ce71a7b4ffc3b53db9de6adbfab20b
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:05:41 -0500

    build (Users.Api): created value object Email

---
+++ Users.Api/Domain/Common/Email.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Text.RegularExpressions;
+using ValueOf;
+
+namespace Users.Api.Domain.Common
+{
+    public class Email : ValueOf<string, Email>
+    {
+        // Regex para validar emails
+        private static readonly Regex EmailRegex =
+            new("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        protected override void Validate()
+        {
+            if (!EmailRegex.IsMatch(Value))
+            {
+                var message = $"{Value} is not a valid email address";
+                throw new ValidationException(message, new[]
+                {
+                new ValidationFailure(nameof(Email), message)
+            });
+            }
+        }
+    }
+}
