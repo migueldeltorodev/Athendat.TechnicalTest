762b00677f54fa3a2b0cde7261a82d1e9c7b14ef
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 18:10:57 -0500

    build (Users.Api): implemented CreateUserEndpoint

---
+++ Users.Api/Endpoints/CreateUserEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using Users.Api.Contracts.Requests;
+using Users.Api.Contracts.Responses;
+using Users.Api.Mapping;
+using Users.Api.Services;
+
+namespace Users.Api.Endpoints
+{
+    public class CreateUserEndpoint(IUserService _userService) : Endpoint<CreateUserRequest, UserResponse>
+    {
+        public override void Configure()
+        {
+            Post("users");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CreateUserRequest req, CancellationToken ct)
+        {
+            var user = req.ToUser();
+
+            await _userService.CreateAsync(user);
+
+            var response = user.ToUserResponse();
+            await SendCreatedAtAsync<GetUserEndpoint>(
+                new { Id = user.Id },
+                response,
+                generateAbsoluteUrl: true,
+                cancellation: ct);
+        }
+    }
+}
