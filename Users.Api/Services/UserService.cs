aec92b4d340091f1d84f7cc92a84b7ea5afa3176
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 23:03:30 -0500

    build (Users.Api): created UserService with CreateAsync method

---
+++ Users.Api/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using Users.Api.Domain;
+using Users.Api.Repositories;
+
+namespace Users.Api.Services
+{
+    public class UserService(IUserRepository _userRepository) : IUserService
+    {
+        public async Task<bool> CreateAsync(User user)
+        {
+            // UserExists?
+            var existingUser = await _userRepository.GetAsync(user.Id.Value);
+            if (existingUser is not null)
+            {
+                throw new ValidationException($"A user with id {user.Id.Value} already exists");
+            }
+            // Email in Use?
+            var users = await _userRepository.GetAllAsync();
+            if (users.Any(x => x.Email.Equals(user.Email.Value)))
+            {
+                throw new ValidationException($"A user with email {user.Email.Value} already exists");
+            }
+
+            return await _userRepository.CreateAsync(user);
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
