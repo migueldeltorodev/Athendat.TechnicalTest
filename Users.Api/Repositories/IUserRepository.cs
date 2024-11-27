75f39adfb590ea69986051b2cb26e8d0e4a5d6a6
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:38:36 -0500

    build (Users.Api): created basic user repository

---
+++ Users.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Users.Api.Domain;
+
+namespace Users.Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<bool> CreateAsync(User user);
+
+        Task<User?> GetAsync(Guid id);
+
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task<bool> UpdateAsync(User user);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
