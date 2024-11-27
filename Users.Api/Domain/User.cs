172e9f791e4c3f21a58a3d230ec7c2b4b1c86a34
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:06:32 -0500

    build (Users.Api): created value object User

---
+++ Users.Api/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using Users.Api.Domain.Common;
+
+namespace Users.Api.Domain
+{
+    public class User
+    {
+        public Id Id { get; init; } = Id.From(Guid.NewGuid());
+        public Username Username { get; init; } = default!;
+        public Email Email { get; init; } = default!;
+        public Password Password { get; init; } = default!;
+    }
+}
