006cad537440abf196fb671ca8e97d13ca957b15
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:54:57 -0500

    build (Users.Api): created basic Users Service interface

---
+++ Users.Api/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Users.Api.Domain;
+
+namespace Users.Api.Services
+{
+    public interface IUserService
+    {
+        Task<bool> CreateAsync(User user);
+
+        Task<User?> GetAsync(Guid id);
+
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task<bool> UpdateAsync(User user);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
