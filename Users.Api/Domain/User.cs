9d741c455e872d023dbd5b2567484ec3b3e40fda
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 22:46:58 -0500

    refactor (Users.Api): added DeleteAsync implementation on user repository

--- Users.Api/Domain/User.cs
+++ Users.Api/Domain/User.cs
@@ -6,7 +6,7 @@ namespace Users.Api.Domain
     {
         public Id Id { get; init; } = Id.From(Guid.NewGuid());
         public Username Username { get; init; } = default!;
-        public Email Email { get; init; } = default!;
         public Password Password { get; init; } = default!;
+        public Email Email { get; init; } = default!;
     }
 }
