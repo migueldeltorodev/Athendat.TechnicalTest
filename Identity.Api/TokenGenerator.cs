bf080f162b58f3321fa8f69f16adc665284c2ae9
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-26 20:36:21 -0500

    build (Identity.Api): created basic token generator

---
+++ Identity.Api/TokenGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Identity.Api
+{
+    public class TokenGenerator
+    {
+        public string GenerateToken(string email)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "ThisIsATechnicalTestForAthendatThatIHopeThatGetMeInto"u8.ToArray();
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, email),
+                new(JwtRegisteredClaimNames.Email, email)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "https://Id.Athendat.com",
+                Audience = "https://Athendat.com",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
