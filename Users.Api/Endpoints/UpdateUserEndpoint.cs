a7a0a9b4b607409679192fca1c661d8ab1425fc8
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 20:44:03 -0500

    build (Users.Api): created new endpoint UpdateUserEndpoint

---
+++ Users.Api/Endpoints/UpdateUserEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FastEndpoints;
+using Users.Api.Contracts.Requests;
+using Users.Api.Contracts.Responses;
+using Users.Api.Mapping;
+using Users.Api.Services;
+
+namespace Users.Api.Endpoints
+{
+    public class UpdateUserEndpoint(IUserService _userService) : Endpoint<UpdateUserRequest, UserResponse>
+    {
+        public override void Configure()
+        {
+            Put("users/{id:guid}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(UpdateUserRequest req, CancellationToken ct)
+        {
+            var user = req.ToUser();
+
+            var updated = await _userService.UpdateAsync(user);
+            if (!updated)
+            {
+                await SendNotFoundAsync(ct);
+                return;
+            }
+
+            var response = user.ToUserResponse();
+            await SendOkAsync(response, ct);
+        }
+    }
+}
