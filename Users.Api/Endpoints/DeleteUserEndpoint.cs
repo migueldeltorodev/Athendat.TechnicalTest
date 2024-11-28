d4d9e1987154f2720ec871c38862a569db8278ea
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 20:43:29 -0500

    build (Users.Api): created new endpoint DeleteUser

---
+++ Users.Api/Endpoints/DeleteUserEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints;
+using Users.Api.Services;
+
+namespace Users.Api.Endpoints
+{
+    public class DeleteUserEndpoint(IUserService _userService) : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Delete("users/{id:guid}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var id = Route<Guid>("id");
+
+            var deleted = await _userService.DeleteAsync(id);
+            if (!deleted)
+            {
+                await SendNotFoundAsync(ct);
+                return;
+            }
+
+            await SendNoContentAsync(ct);
+        }
+    }
+}
