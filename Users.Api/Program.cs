ada2db1a59dda66aead05383c92f188726ae9e27
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 20:47:34 -0500

    refactor (Users.Api): added middleware and pipelines to Program.cs

--- Users.Api/Program.cs
+++ Users.Api/Program.cs
@@ -1,34 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Users.Api.Database;
+using Users.Api.Repositories;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Users.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey("ThisIsATechnicalTestForAthendatThatIHopeThatGetMeInto"u8.ToArray()),
+            ValidIssuer = "https://id.athendat.com",
+            ValidAudience = "https://athendat.com",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+        };
+    });
+
+// FastEndpoints services
+builder.Services
+    .AddFastEndpoints()
+    .SwaggerDocument(o =>
+    {
+        o.DocumentSettings = s =>
+        {
+            s.Title = "Users.Api";
+            s.Version = "v1";
+        };
+    });
+
+// Database services
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+  new MSSQLConnectionFactory(
+    builder.Configuration.GetValue<string>("Database:ConnectionString")!));
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IUserService, UserService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseFastEndpoints();
+
+app.UseOpenApi();
+app.UseSwaggerUi(s => s.ConfigureDefaults());
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+//var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+//await databaseInitializer.InitializeAsync();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
