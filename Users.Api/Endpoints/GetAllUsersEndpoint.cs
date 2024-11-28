fcbbe8324306b2ce7b2826f3fd85f54d58321ba9
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 20:43:47 -0500

    build (Users.Api): created new endpoint GetAllUsersEndpoint

---
+++ Users.Api/Endpoints/GetAllUsersEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using FastEndpoints;
+using Users.Api.Contracts.Responses;
+using Users.Api.Mapping;
+using Users.Api.Services;
+
+namespace Users.Api.Endpoints
+{
+    public class GetAllUsersEndpoint(IUserService _userService) : EndpointWithoutRequest<GetAllUsersResponse>
+    {
+        public override void Configure()
+        {
+            Get("users");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var users = await _userService.GetAllAsync();
+            var response = users.ToUserResponse();
+            await SendOkAsync(response, ct);
+        }
+    }
+}
