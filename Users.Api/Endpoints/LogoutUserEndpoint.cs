318642f9f5280f397e891ba2abc57aad541cb656
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-27 23:39:52 -0500

    build (Users.Api): added a logout endpoint

---
+++ Users.Api/Endpoints/LogoutUserEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Users.Api.Endpoints
+{
+    public class LogoutUserEndpoint : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Post("logout");
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await SendNoContentAsync(ct);
+        }
+    }
+}
